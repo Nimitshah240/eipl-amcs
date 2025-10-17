5fec9949349ed75daddfc92cc01bae707f03bbf3
NimitShah240 <nimitshah240@gmail.com>
2025-10-17 10:51:40 +0530

    completed remaining work

--- src/main/java/com/eipl/amcs/operation/procurement/task/BmcRecordingParameterLoadTask.java
+++ src/main/java/com/eipl/amcs/operation/procurement/task/BmcRecordingParameterLoadTask.java
@@ -1,17 +1,12 @@
 package com.eipl.amcs.operation.procurement.task;
 
-import com.eipl.amcs.MainApp;
 import com.eipl.amcs.config.EmcsAppContext;
 import com.eipl.amcs.operation.procurement.model.BmcRecording;
-import com.eipl.amcs.utils.AppConstant;
+import com.eipl.amcs.operation.procurement.service.BmcRecordingService;
 import javafx.concurrent.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class BmcRecordingParameterLoadTask extends Task<List<BmcRecording>> {
@@ -20,13 +15,11 @@ public class BmcRecordingParameterLoadTask extends Task<List<BmcRecording>> {
     @Override
     protected List<BmcRecording> call() throws Exception {
         try {
-            RestTemplate restTemplate = EmcsAppContext.getContext().getBean(RestTemplate.class);
-            String url = MainApp.getProperty(AppConstant.Props.BASE_URL, null) + AppConstant.UrlPath.RECORDING_PARAMETER;
-            ResponseEntity<BmcRecording[]> response = restTemplate.getForEntity(url, BmcRecording[].class);
-            if (response == null || response.getStatusCode() != HttpStatus.OK)
+            BmcRecordingService service = EmcsAppContext.getContext().getBean(BmcRecordingService.class);
+            List<BmcRecording> list = service.getAllBmcRecordings();
+            if (list == null || list.isEmpty())
                 return null;
-            LOGGER.info("RecordingParameter fetched: {}", response.getBody().length);
-            return Arrays.asList(response.getBody());
+            return list;
         } catch (Exception e) {
             LOGGER.error("RecordingParameter fetch", e);
         }
--- src/main/java/com/eipl/amcs/operation/procurement/task/BmcRunningHrsLoadTask.java
+++ src/main/java/com/eipl/amcs/operation/procurement/task/BmcRunningHrsLoadTask.java
@@ -1,17 +1,12 @@
 package com.eipl.amcs.operation.procurement.task;
 
-import com.eipl.amcs.MainApp;
 import com.eipl.amcs.config.EmcsAppContext;
 import com.eipl.amcs.operation.procurement.model.BmcRunningHours;
-import com.eipl.amcs.utils.AppConstant;
+import com.eipl.amcs.operation.procurement.service.BmcRunningHoursService;
 import javafx.concurrent.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class BmcRunningHrsLoadTask extends Task<List<BmcRunningHours>> {
@@ -27,13 +22,11 @@ public class BmcRunningHrsLoadTask extends Task<List<BmcRunningHours>> {
     @Override
     protected List<BmcRunningHours> call() throws Exception {
         try {
-            RestTemplate restTemplate = EmcsAppContext.getContext().getBean(RestTemplate.class);
-            String url = MainApp.getProperty(AppConstant.Props.BASE_URL, null) + AppConstant.UrlPath.BMC_RUNNING_HRS + "/All";
-            ResponseEntity<BmcRunningHours[]> response = restTemplate.getForEntity(url, BmcRunningHours[].class);
-            if (response == null || response.getStatusCode() != HttpStatus.OK)
+            BmcRunningHoursService service = EmcsAppContext.getContext().getBean(BmcRunningHoursService.class);
+            List<BmcRunningHours> list = service.getAllBmcRunningHours();
+            if (list == null || list.isEmpty())
                 return null;
-            LOGGER.info("BmcRunningHrs fetched: {}", response.getBody().length);
-            return Arrays.asList(response.getBody());
+            return list;
         } catch (Exception e) {
             LOGGER.error("BmcRunningHrs fetch", e);
         }
