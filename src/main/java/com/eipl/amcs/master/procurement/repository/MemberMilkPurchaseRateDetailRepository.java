811baed233f2f9ac9dfacb58902ab26ee2da1020
NimitShah240 <nimitshah240@gmail.com>
2026-01-27 14:03:36 +0530

    Syncing milk collection from desktop api properly with rate and payment cycle

--- src/main/java/com/eipl/amcs/master/procurement/repository/MemberMilkPurchaseRateDetailRepository.java
+++ src/main/java/com/eipl/amcs/master/procurement/repository/MemberMilkPurchaseRateDetailRepository.java
@@ -7,8 +7,11 @@ import com.eipl.amcs.master.procurement.model.MemberMilkPurchaseRateDetail;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,4 +25,20 @@ public interface MemberMilkPurchaseRateDetailRepository extends JpaRepository<Me
     @Override
     @EntityGraph(attributePaths = {"milkType", "milkQualityType", "memberMilkPurchaseRate"})
     Optional<MemberMilkPurchaseRateDetail> findById(String id);
+
+    @Query(value =
+            "SELECT m.code " +
+                    "FROM member_milk_purchase_rate_details d " +
+                    "JOIN member_milk_purchase_rate m " +
+                    "ON d.member_milk_purchase_rate_code = m.code " +
+                    "WHERE d.fat = :fat " +
+                    "AND d.snf = :snf " +
+                    "AND m.wef_date <= CURRENT_DATE " +
+                    "ORDER BY m.wef_date DESC " +
+                    "LIMIT 1", nativeQuery = true)
+    String findRateCode(
+            @Param("fat") BigDecimal fat,
+            @Param("snf") BigDecimal snf
+    );
+
 }
