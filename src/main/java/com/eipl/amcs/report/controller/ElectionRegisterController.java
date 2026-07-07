10d5c77f2184f260615e9961797738a27b129e0a
Dhruv <dhruv.patel@yamatech.in>
2026-07-07 23:07:53 +0530

    Add reports and create fxml

--- src/main/java/com/eipl/amcs/report/controller/ElectionRegisterController.java
+++ src/main/java/com/eipl/amcs/report/controller/ElectionRegisterController.java
@@ -50,23 +50,9 @@ public class ElectionRegisterController implements MyInitialization {
     @FXML
     private ComboBox<String> cboxQtyAmount, cboxLanguage;
     @FXML
-    private ComboBox<Shift> cboxFromShift, cboxToShift;
-    @FXML
     private ComboBox<Shift> cboxFromShift1, cboxToShift1;
     @FXML
     private E_NumericField txtLimit;
-
-    @FXML
-    private DatePicker dpFromDate2, dpToDate2;
-    @FXML
-    private E_NumericField txtLimit2;
-    @FXML
-    private TextField txtqty;
-    @FXML
-    private ComboBox<String> cboxLanguage2;
-    @FXML
-    private Button btnGenerate2, btnClose2;
-
 
     private ResourceBundle resourceBundle;
     private List<MilkType> listMilkType;
@@ -110,36 +96,7 @@ public class ElectionRegisterController implements MyInitialization {
         } else {
             cboxLanguage.setValue("English");
         }
-
-
-        dpFromDate2.setValue(LocalDate.now());
-        dpFromDate2.setConverter(new LocalDateConvertor());
-        dpFromDate2.focusedProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue) {
-                dpFromDate2.setValue(dpFromDate2.getConverter().fromString(dpFromDate2.getEditor().getText()));
-            }
-        });
-        dpToDate2.setValue(LocalDate.now());
-        dpToDate2.setConverter(new LocalDateConvertor());
-        dpToDate2.focusedProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue) {
-                dpToDate2.setValue(dpToDate2.getConverter().fromString(dpToDate2.getEditor().getText()));
-            }
-        });
-        txtLimit2.setText("0");
-        txtqty.setText("0");
-        cboxLanguage2.setItems(FXCollections.observableList(Arrays.asList(arr)));
-        if (MainApp.getLocale().equalsIgnoreCase("gu")) {
-            cboxLanguage2.setValue("Gujarati");
-        } else if (MainApp.getLocale().equalsIgnoreCase("hi")) {
-            cboxLanguage2.setValue("Hindi");
-        } else {
-            cboxLanguage2.setValue("English");
-        }
-
-        btnGenerate2.setOnAction(e -> validateAndGenerateReport1());
-        btnClose2.setOnAction(e -> onCloseElectionRegister1ReportClicked());
-    }
+          }
 
     @Override
     public void setupComboBox() {
@@ -151,15 +108,12 @@ public class ElectionRegisterController implements MyInitialization {
         return cboxLanguage.getSelectionModel().getSelectedItem().substring(0, 2).toLowerCase();
     }
 
-    private String getElectionRegister1LocaleString() {
-        return cboxLanguage2.getSelectionModel().getSelectedItem().substring(0, 2).toLowerCase();
-    }
 
     private void validateAndGenerateReport() {
         Map<String, Object> params = new HashMap<>();
         String localeStr = getLocaleString();
-        params.put("p_from_date", Timestamp.valueOf(dpFromDate.getValue().atTime(cboxFromShift.getValue().getName().equals("Morning") ? 6 : 18, 0)));
-        params.put("p_to_date", Timestamp.valueOf(dpToDate.getValue().atTime(cboxToShift.getValue().getName().equals("Morning") ? 6 : 18, 0)));
+        params.put("p_from_date", Timestamp.valueOf(dpFromDate.getValue().atTime(cboxFromShift1.getValue().getName().equals("Morning") ? 6 : 18, 0)));
+        params.put("p_to_date", Timestamp.valueOf(dpToDate.getValue().atTime(cboxToShift1.getValue().getName().equals("Morning") ? 6 : 18, 0)));
         params.put("p_society_code", MainApp.identityDto.getSociety().getCode());
         params.put("p_animal_type", cboxReportType.getValue().getCode());
         params.put("p_gender_code", cboxGender.getValue().getCode());
@@ -176,25 +130,6 @@ public class ElectionRegisterController implements MyInitialization {
 
         JasperViewer.viewReport(print, false);
     }
-
-    public void validateAndGenerateReport1() {
-        Map<String, Object> params = new HashMap<>();
-        String localeStr = getElectionRegister1LocaleString();
-        params.put(JRParameter.REPORT_LOCALE, new Locale(localeStr));
-        params.put("p_locale", localeStr);
-
-        params.put("p_society_code", MainApp.identityDto.getSociety().getCode());
-        params.put("p_from_date", Timestamp.valueOf(dpFromDate2.getValue().atTime(
-            cboxFromShift.getValue().getName().equals("Morning") ? 6 : 18, 0)));
-        params.put("p_to_date", Timestamp.valueOf(dpToDate2.getValue().atTime(
-            cboxToShift.getValue().getName().equals("Morning") ? 6 : 18, 0)));
-        params.put("p_no_of_days", Integer.parseInt(txtLimit2.getInputText().trim()));
-        params.put("p_qty", Integer.parseInt(txtqty.getText().trim()));
-
-        JasperPrint print = ReportGenerate.getReportDataSourceJasperPrint(AppConstant.ReportPath.ELECTION_REGISTER_ONE, params);
-        JasperViewer.viewReport(print, false);
-    }
-
     public void onCloseElectionRegister1ReportClicked() {
         MainApp.getContentPane().setCenter(MainApp.getFxmlLoaderUtil().load(MainApp.class.getResource("view/dashboard/Dashboard.fxml")));
     }
@@ -229,10 +164,6 @@ public class ElectionRegisterController implements MyInitialization {
                     cboxFromShift1.getSelectionModel().select(0);
                     cboxToShift1.setItems(FXCollections.observableList(CommonUtils.removeAllShift(list)));
                     cboxToShift1.getSelectionModel().select(1);
-                    cboxFromShift.setItems(FXCollections.observableList(CommonUtils.removeAllShift(list)));
-                    cboxFromShift.getSelectionModel().select(0);
-                    cboxToShift.setItems(FXCollections.observableList(CommonUtils.removeAllShift(list)));
-                    cboxToShift.getSelectionModel().select(1);
                 }
             } catch (InterruptedException | ExecutionException ex) {
                 ex.printStackTrace();
